9afc6538042daf8d8e14c8c44b9644ab1c25eb3f
Jedaiah De La Cruz <kingjedi20@gmail.com>
2023-04-17 14:53:11 -0400

    Javascript intro

---
+++ Grade.js
@@ -0,0 +1,16 @@
+let myGrade = 69
+if (myGrade >= 90 && myGrade <= 100){
+    console.log("A")
+}
+if (myGrade >= 80 && myGrade < 90){
+    console.log("B")
+}
+if (myGrade >= 70 && myGrade < 80){
+    console.log("C")
+}
+if (myGrade >= 60 && myGrade <70){
+    console.log("D")
+}
+if (myGrade >= 0 && myGrade < 60){
+    console.log("F")
+}
---
+++ Helllo.js
@@ -0,0 +1,24 @@
+console.log("hello world")
+let myName = "Jedaiah D"
+let myAge = 16
+let notMyName = "Jonthan D"
+console.log("Hello my name is " + myName + ". My age is " + myAge)
+console.log("Does 3 equal 3?")
+console.log("If it does that means:")
+if (3 == 3){
+    console.log("Math is correct")
+}
+console.log("Will ms M let me sleep in class even though I did my work?")
+if (10 == 10){
+    console.log("No")
+}
+if (myName == "Jedaiah D"){
+    console.log("I am the real Jedaiah D.")
+} else {
+    console.log("I am not the real Jedaiah D.")
+}
+console.log("Here are the modern companies that own consoles.")
+let myList = ["Microsoft", "sony", "Nintendo",]
+for (let item of myList){
+    console.log(item)
+}
